0b2b6cb0db2d81fd0fb171cd5e7ad83d5ae80eb3
Kamil Polanski <polanski88@gmail.com>
2019-07-23 22:53:24 +0200

    End of task 25.3?

--- index.js
+++ index.js
@@ -1,33 +1,8 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+let stringifyFile;
 
-
-app.get('/', (req, res) => {
-    console.log('Otrzymałem żądanie GET do strony głównej');
-    res.send(`Identyfikator, który został dopisany to  ${req.params.id}`);
-});
-
-
-app.post('/', (req, res) => {
-    console.log('Otrzymałem żądanie POST do strony głównej');
-    res.send('Hello POST!');
-});
-
-app.delete('/del_user', (req, res) => {
-    console.log('Otrzymałem żądanie DELETE do strony /del_user');
-    res.send('Hello DELETE!');
-});
-
-app.get('/list_user', (req, res) => {
-    console.log('Otrzymałem żądanie GET do strony /list_user');
-    res.send('Strona z listą użytkowników!');
-});
-
-app.post('/ab*cd', (req, res) => {
-    console.log('Otrzymałem żądanie GET do strony /ab*cd');
-    res.send('Wzór pasuje');
-});
 
 app.get('/getnote', (req, res) => {
     console.log('pobbieram get note');
@@ -38,7 +13,16 @@ app.get('/getnote', (req, res) => {
     });
 });
 
-app.use(function(req, res, next) {
+app.post('/updateNote/:note', (req, res) => {
+    console.log('wysyłam note!');
+    stringifyFile += req.params.note;
+    fs.writeFile('./test.json', stringifyFile, (err) => {
+        if (err) throw err;
+        console.log('file updated');
+    });
+});
+
+app.use((req, res, next) => {
     res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
 });
 
