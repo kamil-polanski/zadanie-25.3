27e14bcdbf1203a3e67e82c870423f873e5c1f7e
Kamil Polanski <polanski88@gmail.com>
2019-08-28 17:43:48 +0200

    End of task

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('assets'));
+app.use('/store',(req, res, next) => {
+    console.log('Jestem pośrednikiem przy żądaniu do /store');
+    next();
+});
 
 app.get('/', (req, res) => {
     res.sendFile('/index.html')
+});
+
+app.get('/store', (req, res) => {
+    res.send('To jest sklep')
 });
 
 app.get('/userform', (req, res) => {
@@ -19,6 +27,5 @@ app.get('/userform', (req, res) => {
 const server = app.listen(9000, 'localhost', () => {
     const host = server.address().address;
     const port = server.address().port;
-
     console.log(`Przykładowa aplikacja nasłuchuje na http://${host}:${port}`);
 });
